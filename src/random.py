1202db76854e662b9fc08f67934a54502e9dd4a6
MrDomian <41548647+MrDomian@users.noreply.github.com>
2022-09-04 16:28:02 +0200

    Create random.py

---
+++ src/random.py
@@ -0,0 +1,10 @@
+import random
+
+# Random number
+random_number = random.randrange(1000000)
+
+# Random comfort words, when person is sad
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+comfort_words = ["Cheer up!", "Hang in there.", "You are a great person!"]
+
+comfort_word = random.choice(comfort_words)
